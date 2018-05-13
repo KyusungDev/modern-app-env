265ed411dc6bc1f3d8681aee70d370ebacde1652
paul <paul@novonetworks.com>
2018-05-13 22:13:12 +0900

    init

---
+++ index.js
@@ -0,0 +1,6 @@
+import moment from 'moment';
+
+console.log("Hello from JavaScript!");
+console.log(moment().startOf('day').fromNow());
+var name = "Bob1", time = "today";
+console.log(`1e1sdggggfsdf1llo ${name}, how are you ${time}?`);
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist/',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      }
+    ]
+  }
+};
